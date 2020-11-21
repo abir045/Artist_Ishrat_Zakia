c9dd1546f7fa47fea2692a4f33220822b62740ac
Salekin Imran <abir045@gmail.com>
2020-11-21 17:31:50 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+
+
+
+// $(document).ready(function(){
+//     var posX = window.innerWidth;
+//     var posY = window.innerHeight;
+//     var posXH = window.innerWidth /2;
+//     var posYH = window.innerHeight /2;
+
+// function imageRandomSlide(){
+//     new TimelineMax()
+//    .to('.image-area',1,{
+//        opacity:1,
+//        scale:1,
+//        ease: Power2.easeout,
+//        x:function(){return Math.random()* posX},
+//        y:function(){return Math.random()* posY}
+//     })
+
+// }
+// imageRandomSlide()
+
+// })
+
+
+let imgArr = document.querySelectorAll(".img");
+
+
